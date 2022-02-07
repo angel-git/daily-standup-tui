2689ec53966aed5cb0f2dc057fbee90539dfb296
agavalda <agavalda@digital.ai>
2022-02-07 22:15:06 +0100

    feat(UI): Add and delete devs

--- src/api/files.rs
+++ src/api/files.rs
@@ -1,16 +1,21 @@
 use std::fs;
-use std::fs::File;
-use std::io::Write;
+use std::path::PathBuf;
 use serde::Deserialize;
 use serde::Serialize;
 
 const CONFIG_FILE: &str = "config.toml";
 const APP_DIR: &str = ".daily-standup-tui";
 
+pub struct Files {
+    config_file: PathBuf,
+    config_app: FileConfig,
+}
+
+
 #[derive(Deserialize, Serialize)]
-pub struct FileConfig {
+struct FileConfig {
     devs: DevelopersConfig,
-    settings: Option<SettingsConfig>
+    settings: Option<SettingsConfig>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -24,21 +29,22 @@ struct SettingsConfig {
 }
 
 
-impl FileConfig {
-
-    pub fn new() -> FileConfig {
-
-        let mut file_config = FileConfig {
-            devs: DevelopersConfig {
-                names: Vec::from(["Example Dev 1", "Example Dev 2"]).into_iter().map(String::from).collect()
-            },
-            settings: Some(SettingsConfig {
-                seconds: 180
-            }),
+impl Files {
+    pub fn new() -> Files {
+        let mut files = Files {
+            config_file: PathBuf::new(),
+            config_app: FileConfig {
+                devs: DevelopersConfig {
+                    names: Vec::from(["Example Dev 1", "Example Dev 2"]).into_iter().map(String::from).collect()
+                },
+                settings: Some(SettingsConfig {
+                    seconds: 180
+                }),
+            }
         };
 
-        file_config.load_file_config();
-        file_config
+        files.load_file_config();
+        files
     }
 
     fn load_file_config(&mut self) {
@@ -58,33 +64,49 @@ impl FileConfig {
         }
 
         let config_file = app_dir_in_home_folder.join(CONFIG_FILE);
+        self.config_file = config_file.clone();
 
         if !config_file.exists() {
-            // let file = File::create(config_file);
-            let toml_string = toml::to_string(self).expect("Can't parse toml default values");
-            fs::write(config_file, toml_string);
+            self.write_toml_file();
         } else {
             let toml_string = fs::read_to_string(config_file).unwrap();
-            match toml::from_str::<FileConfig>(toml_string.as_str())  {
+            match toml::from_str::<FileConfig>(toml_string.as_str()) {
                 Ok(conf) => {
-                    self.settings = conf.settings;
-                    self.devs = conf.devs;
-                },
+                    self.config_app.settings = conf.settings;
+                    self.config_app.devs = conf.devs;
+                }
                 Err(e) => panic!("Error while parsing config file {}", e)
             }
         }
     }
 
-    pub fn get_devs(&self) -> &Vec<String> {
-        return &self.devs.names;
+    fn write_toml_file(&self) {
+        let toml_string = toml::to_string(&self.config_app).expect("Can't parse toml default values");
+        match fs::write(self.config_file.clone(), toml_string) {
+            Ok(_) => (),
+            Err(e) => panic!("Error while writing config file {}", e)
+        }
     }
 
+    pub fn get_devs(&self) -> &Vec<String> {
+        return &self.config_app.devs.names;
+    }
 
+    pub fn add_dev(&mut self, name: &str) {
+        let names = &mut self.config_app.devs.names;
+        names.push(name.to_string());
+        self.write_toml_file();
+    }
 
+    pub fn delete_dev(&mut self, index: usize) {
+        let names = &mut self.config_app.devs.names;
+        names.remove(index);
+        self.write_toml_file();
+    }
 }
 
 
-impl Default for FileConfig {
+impl Default for Files {
     fn default() -> Self {
         Self::new()
     }
