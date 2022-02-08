0a555a30871ae603f9039b00b2997f2e6624cf14
agavalda <agavalda@digital.ai>
2022-02-08 08:48:17 +0100

    feat(UI): Next developer turn

--- src/ui/config.rs
+++ src/ui/config.rs
@@ -1,8 +1,12 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 use crate::api::files::Files;
 
 pub struct ConfigApi {
     started_daily: bool,
     file_config: Files,
+    devs_turn: Vec::<String>
 }
 
 impl ConfigApi {
@@ -12,10 +16,15 @@ impl ConfigApi {
         ConfigApi {
             started_daily: false,
             file_config,
+            devs_turn: Vec::new(),
         }
     }
 
     pub fn start(&mut self) {
+        // randomize the turn
+        let mut devs = self.file_config.get_devs().to_vec();
+        devs.shuffle(&mut thread_rng());
+        self.devs_turn = devs;
         self.started_daily = true;
     }
 
@@ -28,11 +37,25 @@ impl ConfigApi {
     }
 
     pub fn add_dev(&mut self, name: &str) {
+        if *self.is_started() {
+            panic!("Can't modify devs when daily has started");
+        }
         self.file_config.add_dev(name);
     }
 
     pub fn delete_dev(&mut self, index: usize) {
+        if *self.is_started() {
+            panic!("Can't modify devs when daily has started");
+        }
         self.file_config.delete_dev(index);
+    }
+
+    pub fn get_turns(&self) -> &Vec::<String> {
+        return &self.devs_turn;
+    }
+
+    pub fn next(&mut self) {
+        self.devs_turn.remove(0);
     }
 }
 
